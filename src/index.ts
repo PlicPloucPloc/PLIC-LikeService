180b837cac4f344b93d030c93b44df6453ccc666
Babouche007 <baptiste.perrier@epita.fr>
2025-05-05 08:20:48 +0200

    Create endpoints

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Elysia } from "elysia";
+import { likeRoutes } from "./routes";
+import swagger from "@elysiajs/swagger";
+
+const app = new Elysia();
+
+app
+    .group("", (app) => app.use(likeRoutes))
+    .use(swagger())
+    .listen(process.env.relationPORT || 3000);
+
---
+++ src/routes/index.ts
@@ -0,0 +1,71 @@
+import Elysia, { t } from "elysia";
+import { bearer } from "@elysiajs/bearer";
+
+const likeRoutes = new Elysia({prefix: "/relations"});
+
+likeRoutes.use(bearer()).get('/all', ({ bearer }) => bearer, {
+    beforeHandle({ bearer, set, error }) {
+        if (!bearer) {
+            set.headers[
+                'WWW-Authenticate'
+            ] = `Bearer realm='sign', error="invalid_request"`
+
+            return error(400, 'Unauthorized')
+        }
+    }
+})
+
+likeRoutes.use(bearer()).get('/likes/:isFilterColoc', ({ bearer }) => bearer, {
+    beforeHandle({ bearer, set, error }) {
+       if(!bearer) {
+            set.headers[
+                'WWW-Authenticate'
+            ] = `Bearer realm='sign', error="invalid_request"`
+
+            return error(400, 'Unauthorized')
+       } 
+    }
+})
+
+likeRoutes.use(bearer()).get('/dislikes', ({ bearer }) => bearer, {
+    beforeHandle({ bearer, set, error }) {
+        if(!bearer) {
+            set.headers[
+                'WWW-Authenticate'
+            ] = `Bearer realm='sign', error="invalid_request"`
+            return error(400, 'Unauthorized')
+        }
+    }
+})
+
+likeRoutes.use(bearer()).post('/', ({ bearer, body }) => bearer + body, {
+    body : t.Object({
+        aptId: t.String(),
+        isLike: t.Boolean()
+    }),
+    beforeHandle({ bearer, set, error }) {
+        if (!bearer) {
+            set.headers[
+                'WWW-Authenticate'
+            ] = `Bearer realm='sign', error="invalid_request"`
+            return error(400, 'Unauthorized')
+        }
+    }
+})
+
+likeRoutes.use(bearer()).put('/', ({ bearer, body }) => bearer + body, {
+    body : t.Object({
+        aptId: t.String(),
+        isLike: t.Boolean()
+    }),
+    beforeHandle({ bearer, set, error }) {
+        if (!bearer) {
+            set.headers[
+                'WWW-Authenticate'
+            ] = `Bearer realm='sign', error="invalid_request"`
+            return error(400, 'Unauthorized')
+        }
+    }
+})
+
+export {likeRoutes};
