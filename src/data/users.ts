526c83fcf650ad3199584608c35cfaf4cd7d6ae1
Babouche007 <baptiste.perrier@epita.fr>
2025-06-14 16:02:05 +0200

    Fix errors

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,7 +1,6 @@
 import { HttpError } from "elysia-http-error";
 
 async function getUser(bearer : String) : Promise<string> {
-    console.log("Getting user")
     const userUrl = (process.env.USER_URL || "http://localhost:3000") + "/user/id"
     const request = new Request(userUrl, {
         method: "get",
@@ -11,10 +10,11 @@ async function getUser(bearer : String) : Promise<string> {
         }
     })
     const resp = await fetch(request);
-    if (!resp){
+    if (!resp || resp.status == 403){
         throw HttpError.Forbidden("User not found or acces denied")
     }
     const content = await resp.json();
+    console.log("Content: " + content);
     if (content === null) {
         throw HttpError.Forbidden("User not found or acces denied");
     }
