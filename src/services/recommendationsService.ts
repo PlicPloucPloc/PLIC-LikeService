b0b28c2d0652acaf4aee6cbcca3926438dadb2d7
Babouche007 <baptiste.perrier@epita.fr>
2025-11-10 10:15:16 +0100

    Clean post refacto

--- src/services/recommendationsService.ts
+++ src/services/recommendationsService.ts
@@ -1,9 +1,6 @@
 import { Logger } from "winston";
 import { checkSimilarityGraph, dropOldRelationsGraph, generateRelationsGraph, generateSimilarityGraph, getSimilarUsers, getSimilarUsersColloc, orderByProximity } from "../data/recommendations";
-import { fetchApartmentWithZeroRelations } from "../data/relations";
 import { getLogger } from "./logger";
-import { getAllLikes } from "./likeService";
-import { relation } from "../models/relation";
 import { handleResponse } from "./responseService";
 
 const logger: Logger = getLogger('Recommendation');
@@ -17,27 +14,6 @@ export async function generateRecommendations(): Promise<void> {
 export async function fetchSimilarUsers(id: string): Promise<string[]> {
         const records = await getSimilarUsers(id);
         return records.map(record => record.get('Person2'));
-}
-
-async function getUserLikes(user: string, recommendedApts: string[], aptRel: string[], bearer: string, previousZeroRelAptsCount: number): Promise<string[]>{
-        logger.info(`Fetching likes for similar user: ${user}`);
-        const apartments: relation[] = await getAllLikes(bearer, user, 0, 10);
-        logger.info('Likes found');
-        var aptsAdded: number = 0;
-        apartments.forEach((apt) => {
-            const aptId = apt.apt.apartment_id.toString();
-            if (!recommendedApts.includes(aptId) && !aptRel.includes(aptId)){
-                aptsAdded++;
-                recommendedApts.push(aptId);
-                logger.info(`Added apartment to recommendations: ${aptId}`);
-            }
-        });
-        if (aptsAdded < 10) {
-            (await fetchApartmentWithZeroRelations(previousZeroRelAptsCount,10 - aptsAdded))
-            .map((apt) => recommendedApts.push(apt.get('a').properties.id));
-        }
-        previousZeroRelAptsCount += (10 - aptsAdded);
-        return recommendedApts;
 }
 
 export async function getRecommendedColloc(userId: string, skip: number, limit: number): Promise<string[]> {
--- src/services/userVerificationService.ts
+++ src/services/userVerificationService.ts
@@ -8,7 +8,6 @@ const logger: Logger = getLogger('User');
 export async function verifyUser(bearer: string) : Promise<string> {
     logger.info('Getting User');
     const userId = await getUser(bearer);
-    logger.info('Got User');
     if (!userId) {
         logger.error('User not found');
         throw HttpError.Unauthorized('User do not exist');
